81f1bfc69de5069d7a9efc2d60c5d24c7a4572be
gimmeabeer <53146540+krastomer@users.noreply.github.com>
2021-09-15 15:20:07 +0700

    update bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -17,7 +17,7 @@ void main()
           strcmp(G, "B") != 0 &&
           strcmp(G, "AB") != 0 &&
           strcmp(G, "O") != 0)
-        printf("Blood group %s is incorrect! Please try again.\n", G);
+        printf("Blood group %s is incorrect! Please let do it again.\n", G);
     } while (strcmp(G, "A") != 0 &&
              strcmp(G, "B") != 0 &&
              strcmp(G, "AB") != 0 &&
@@ -45,5 +45,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("GoodLuck\n");
+  printf("GoodLuck , And Have fun\n");
 }
